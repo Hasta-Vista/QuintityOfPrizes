cc5ce96c12f46d6b5abb3591ea41c50252d6207b
Hasta-Vista <ovchinnikov675@yandex.com>
2024-07-30 23:53:46 +0400

    Binar

---
+++ Candies.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+long long Places(long long candies) {
+    long long used = 0, half, maxPlaces = 0;
+
+    while (used <= candies) {
+        half = used + (candies - used) / 2;
+        long long sum = half * (half + 1) / 2;
+
+        if (sum <= candies) {
+            maxPlaces = half;
+            used = half + 1;
+        }
+        else {
+            candies = half - 1;
+        }
+    }
+
+    return maxPlaces;
+}
+
+int main() {
+    long long candies;
+    cin >> candies;
+
+    cout << Places(candies) << endl;
+
+    return 0;
+}
+    
